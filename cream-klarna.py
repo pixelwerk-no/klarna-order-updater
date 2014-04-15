aef756419342b849ff62520882791b7446b5efb0
Luciano Leveroni <lucianolev@gmail.com>
2014-04-15 17:04:18 -0300

    Initial commit

---
+++ cream-klarna.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import argparse
+import klarnacheckout
+
+parser = argparse.ArgumentParser(description='Klarna command line client.')
+parser.add_argument('--acknowledge-order', dest='checkout_id', action='store', nargs=1,
+                    help='acknowledge order with specified checkout id')
+
+args = parser.parse_args()
+
+checkout_id = args.checkout_id[0]
+
+klarnacheckout.Order.base_uri = 'https://checkout.testdrive.klarna.com/checkout/orders'
+klarnacheckout.Order.content_type = 'application/vnd.klarna.checkout.aggregated-order-v2+json'
+
+connector = klarnacheckout.create_connector('shared_secret')
+order = klarnacheckout.Order(connector, checkout_id)
+order.fetch()
+
+if order['status'] == 'checkout_complete':
+    update_data = {'status': 'created'}
+
+    order.update(update_data)
+else:
+    raise Exception(
+        "The checkout order with id %s is not complete. Aborting. Order status will NOT be updated." % checkout_id)
